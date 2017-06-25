c1ebba55dcbbe6587ee7093bc8f236931be07781
Tsvetoslav Tsvetanov <tsvetoslavtsvetanov@deltasource.eu>
2017-06-25 22:17:41 +0300

    created repository for User

---
+++ src/main/java/com/friendlybear/worldinpictures/repositories/UserRepository.java
@@ -0,0 +1,18 @@
+package com.friendlybear.worldinpictures.repositories;
+
+import com.friendlybear.worldinpictures.domain.user.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Spring REST repository for user
+ * <p>
+ * Created by Tsvetoslav Tsvetanov - Delta Source Bulgaria on 6/25/17.
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findUserBId(long id);
+
+    User finfUserByUsername(String username);
+}
