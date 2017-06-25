0650923d1462b8584e308b91e9b54f31a60b38b0
Tsvetoslav Tsvetanov <tsvetoslavtsvetanov@deltasource.eu>
2017-06-25 22:44:49 +0300

    added some lines in Spring  Application class

--- src/main/java/com/friendlybear/worldinpictures/WorldInPicturesApplication.java
+++ src/main/java/com/friendlybear/worldinpictures/WorldInPicturesApplication.java
@@ -1,12 +1,28 @@
 package com.friendlybear.worldinpictures;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class WorldInPicturesApplication {
 
+	@Value("${environment.name}")
+	private String environmentName;
+
+	private static final Logger LOG = LoggerFactory.getLogger(WorldInPicturesApplication.class);
+
+
 	public static void main(String[] args) {
 		SpringApplication.run(WorldInPicturesApplication.class, args);
+	}
+
+	@PostConstruct
+	public void postConstruct(){
+		LOG.info("Application initialized. Environment = {}", environmentName);
 	}
 }
