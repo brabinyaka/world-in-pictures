76adc1e79348f1cb341dd4cf0a8aa4ba83aec185
Tsvetoslav Tsvetanov <tsvetoslavtsvetanov@deltasource.eu>
2017-06-25 21:17:13 +0300

    initial commit

---
+++ src/main/java/com/friendlybear/worldinpictures/WorldInPicturesApplication.java
@@ -0,0 +1,12 @@
+package com.friendlybear.worldinpictures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldInPicturesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorldInPicturesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/friendlybear/worldinpictures/WorldInPicturesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.friendlybear.worldinpictures;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WorldInPicturesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
